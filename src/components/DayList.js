82ac12e9fb636db992ce983aa204150948d113aa
mclmnop <m_charlebois@hotmail.com>
2021-03-15 18:18:12 -0400

    buttons and DayListItem completed

---
+++ src/components/DayList.js
@@ -0,0 +1,4 @@
+//Takes three props>>
+// days:Array a list of day objects (each object includes an id, name, and spots)
+// day:String the currently selected day
+// setDay:Function accepts the name of the day eg. "Monday", "Tuesday"
---
+++ src/components/DayListItem.jsx
@@ -0,0 +1,40 @@
+/* name:String the name of the day
+spots:Number the number of spots remaining
+selected:Boolean true or false declaring that this day is selected
+setDay:Function accepts the name of the day eg. "Monday", "Tuesday" */
+
+import React from "react";
+import classnames from 'classnames';
+import "components/DayListItem.scss";
+
+
+const formatSpots = (spotsNumber) => {
+  let message = ""
+  if (spotsNumber === 1) {
+    message = "1 spot remaining"
+  } else if (spotsNumber === 0) {
+    message = "no spots remaining"
+  } else {
+  message = `${spotsNumber} spots remaining`
+  }
+  return message;
+}
+
+export default function DayListItem(props) {
+  
+ /*  day-list__item all the time
+  day-list__item--selected class name if props.selected is true
+  day-list__item--full class name if props.spots is 0. */
+  const dayClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+  const spotsFormatted = formatSpots(props.spots)
+
+  return (
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
+        <h2 className="text--regular">{props.name}</h2> 
+        <h3 className="text--light">{spotsFormatted}</h3>
+    </li>
+  );
+}
