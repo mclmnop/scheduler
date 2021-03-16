be889f48121cbb831498f3bae1cec35b1665d292
mclmnop <m_charlebois@hotmail.com>
2021-03-16 16:35:19 -0400

    basic appointment story added

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,7 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/index";
 
 storiesOf("Button", module)
   .addParameters({
@@ -155,3 +156,13 @@ storiesOf("InterviewerList", module)
     />
   ));
 
+storiesOf("Appointment", module)
+    .addParameters({
+      backgrounds: [{name:"white", value: "#fff", default:true}]
+    })
+    .add("Appointment", () => <Appointment/>)
+    .add("Appointment with time", () => (
+    <Appointment
+    time="12pm"
+    />
+  ))
